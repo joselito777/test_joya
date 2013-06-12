b9de612f1846616b3b1a323d4b4cfe4a198ad6be
jesus <jesus@samsung.(none)>
2013-06-12 09:37:53 -0300

    maqueta nivel 0

---
+++ www/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<title>Piaggio</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<link rel="stylesheet" href="stylesheets/base.css">
+	<link rel="stylesheet" href="stylesheets/piaggio.css">
+	<link rel="stylesheet" href="stylesheets/skeleton.css">
+	<link rel="stylesheet" href="stylesheets/layout.css">
+
+	<script src="scripts/jquery-1.10.1.min.js"></script>
+	<script src="scripts/jquery.ez-bg-resize.js"></script>
+
+	<script src="scripts/functions.js"></script>
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+
+</head>
+<body>
+	<div id="wrapper">
+		<div id="header"><div class="container">header</div></div>
+		<div class="container">
+			<nav class="columns three">
+				<ul>
+					<li>
+						<a href="#">Secction 1</a>
+						<ul>
+							<li><a href="#">Subsection 1 1</a></li>
+							<li><a href="#">Subsection 1 2</a></li>
+							<li><a href="#">Subsection 1 3</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">Secction 2</a>
+						<ul>
+							<li><a href="#">Subsection 2 1</a></li>
+							<li><a href="#">Subsection 2 2</a></li>
+							<li><a href="#">Subsection 2 3</a></li>
+							<li><a href="#">Subsection 2 4</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">Secction 3</a>
+						<ul>
+							<li><a href="#">Subsection 3 1</a></li>
+							<li><a href="#">Subsection 3 2</a></li>
+							<li><a href="#">Subsection 3 3</a></li>
+							<li><a href="#">Subsection 3 4</a></li>
+						</ul>
+					</li>
+				</ul>
+			</nav>
+
+			
+			<section class="columns eight right">
+				Wenn man von einem Geschäft behaupten kann, dass es mit dem Herzen geführt wird, so trifft dies auf
+uns zu. Seit mehr als zehn Jahren pflegen wir konsequent die alte Tradition des Goldschmiedehandwerks
+mit mehr als nur dem Blick auf den wirtschaftlichen Erfolg. Die permanente Auseinandersetzung mit Platin- und Goldschmiedetechniken führte uns zu einem eigenen experimentierfreudigen und kreativen Stil.
+Diese Elemente bilden unsere Philosophie, die allen Ansprüchen gerecht wird.
+Alljährlich laden wir unsere Kunden zu aktuellen Vorträgen ein und informieren sie ausführlich über Perlen und Diamanten. Für diese Veranstaltungen konnten wir bereits bekannte Persönlichkeiten unserer
+Branche gewinnen. Neben Elisabeth Strack, Hans Schoeffel und Gaby Tolkowsky fanden sich auch Jeanette Fiedler, Karl Mayer Amler, Erhard Brepohl und Gregor Schwarz bei uns als Referenten ein ebenso wie
+Nicolas Piaggio, der seine Beiträge mit persönlichem Dokumentationsmaterial veranschaulichen konnte:
+Bisher sind bereits zwei Publikationen erschienen: - "Aus dem Feuer in das Licht" über die Argyle Diamanten-Mine in Kununurra Australien (Göttingen 1998) und - "Streifzüge ins Licht" über die Südseeperlenzucht (Göttingen 1999).
+Über die Zucht von Südseeperlen steht Ihnen auch ein Film unter dem gleichnamigen Titel aus eigener
+Produktion (Göttingen 1999) zur Verfügung, um die unterschiedlichen Bedingungen und Zuchtverfahren
+auszuführen.
+Nicolas Piaggio
+(*1954, verheiratet, drei Kinder) ist Gutachter für Diamanten und Spezialist rund um das Thema Perlen.
+Seine zahlreichen Reisen zu verschiedenen Diamantfundorten und in diverse Perlenzuchtgebiete belegen
+ein solides Sachverständnis. In seiner Funktion als Unterwasser-Fotograf wie auch als Journalist arbeitet
+			</section>
+
+
+
+		</div>
+	</div>
+</body>
+</html>
---
+++ www/scripts/functions.js
@@ -0,0 +1,16 @@
+$(function(){
+	$("body").ezBgResize({
+		img : "images/layout/fusion-del-oro-624023.jpg",
+		opacity : 1, 
+		center  : true 
+	});
+
+	$('nav ul li').first().css('border-top', '1px solid #ffffff');
+
+	$('nav ul li').bind('mouseenter', function() {
+		$(this).children('ul').stop().slideDown();
+	});
+	$('nav ul li').bind('mouseleave', function() {
+		$(this).children('ul').stop().slideUp();
+	});
+})
