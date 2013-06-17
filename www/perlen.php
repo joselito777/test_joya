2997fb3c583c8a653d5d9e0b5af82c1552480390
jesus <jesus@samsung.(none)>
2013-06-17 09:34:03 -0300

    seccion perlen

---
+++ www/perlen.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<title>GOLDSCHMIEDE ATELIER PIAGGIO</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<link rel="stylesheet" href="stylesheets/base.css">
+	<link rel="stylesheet" href="stylesheets/piaggio.css">
+	<link rel="stylesheet" href="stylesheets/skeleton.css">
+	<link rel="stylesheet" href="stylesheets/layout.css">
+
+	<script src="scripts/jquery-1.10.1.min.js"></script>
+	<script src="scripts/jquery.ez-bg-resize.js"></script>
+
+	<script src="scripts/functions.js"></script>
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<div id="wrapper">
+		<div id="header">
+			<div class="container">
+				<h1><a href="index.php" class="logo" title="Piaggio">GOLDSCHMIEDE ATELIER PIAGGIO</a></h1>
+				<div class="links">
+					<a href="http://www.goldschmiede-piaggio-shop.com" target="_blank" title="">www.goldschmiede-piaggio-shop.com</a> · 
+					<a href="http://www.goldschmiede-shop.com" target="_blank" title="">www.goldschmiede-shop.com</a> · 
+					<a href="http://www.nicolas-piaggio-fotograf.de" target="_blank" title="">www.nicolas-piaggio-fotograf.de</a><br />
+					<a href="#" target="_blank" title="">FACEBOOK</a> - 
+					<a href="#" target="_blank" title="">BLOG</a> - 
+					<a href="#" target="_blank" title="">IMPRESSUM</a>
+				</div>
+				<h2>
+					GOLDSCHMIEDEATELIER PIAGGIO<br />
+					ERLEBNISSE IN GOLD & PLATIN
+				</h2>				
+				<h3>
+					THEATERSTRASSE 23 · 37073 GÖTTINGEN<br />
+					TELEFON 0551 - 541136 · <a href="mailto:npiaggio@goldschmiede-piaggio.de">npiaggio@goldschmiede-piaggio.de</a>
+				</h3>
+			</div>
+		</div>
+		<div class="container">
+			<nav class="columns three">
+				<ul>
+					<li>
+						<a href="perlen.php">HOME</a>
+					</li>
+					<li class="active">
+						<a class="active" href="perlen.php">PERLEN</a>
+						<ul>
+							<li><a class="active" href="perlen.php">TAHITI-PERLEN</a></li>
+							<li><a href="perlen.php">SÜDSEE-PERLEN</a></li>
+							<li><a href="perlen.php">AKOYA-PERLEN</a></li>
+							<li><a href="perlen.php">CHIN. SÜDWASER-PERLEN</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="perlen.php">DIAMENTEN 3</a>
+						<ul>
+							<li><a href="perlen.php">DIAMENTEN 3 1</a></li>
+							<li><a href="perlen.php">DIAMENTEN 3 2</a></li>
+							<li><a href="perlen.php">DIAMENTEN 3 3</a></li>
+							<li><a href="perlen.php">DIAMENTEN 3 4</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="perlen.php">FARBEDELSTEINE 3</a>
+						<ul>
+							<li><a href="perlen.php">FARBEDELSTEINE 3 1</a></li>
+							<li><a href="perlen.php">FARBEDELSTEINE 3 2</a></li>
+							<li><a href="perlen.php">FARBEDELSTEINE 3 3</a></li>
+							<li><a href="perlen.php">FARBEDELSTEINE 3 4</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="perlen.php">PLATIN + GOLD 3</a>
+						<ul>
+							<li><a href="perlen.php">PLATIN + GOLD 3 1</a></li>
+							<li><a href="perlen.php">PLATIN + GOLD 3 2</a></li>
+							<li><a href="perlen.php">PLATIN + GOLD 3 3</a></li>
+							<li><a href="perlen.php">PLATIN + GOLD 3 4</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="perlen.php">ANGEBOTE 3</a>
+						<ul>
+							<li><a href="perlen.php">ANGEBOTE 3 1</a></li>
+							<li><a href="perlen.php">ANGEBOTE 3 2</a></li>
+							<li><a href="perlen.php">ANGEBOTE 3 3</a></li>
+							<li><a href="perlen.php">ANGEBOTE 3 4</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="perlen.php">KONTAKT 3</a>
+						<ul>
+							<li><a href="perlen.php">KONTAKT 3 1</a></li>
+							<li><a href="perlen.php">KONTAKT 3 2</a></li>
+							<li><a href="perlen.php">KONTAKT 3 3</a></li>
+							<li><a href="perlen.php">KONTAKT 3 4</a></li>
+						</ul>
+					</li>
+				</ul>
+			</nav>
+
+			
+			<section class="columns sectionContent eleven right">
+				<h3>Tahiti-Perlen</h3>
+				In den Tiefen der Lagunen des Tuamoto Archipels, abgelegen der Zivilisation der Cook Islands oder den
+				Südprovinzen Chinas, werden die sogenannten "Schwarzen Perlen" gezüchtet. Nach jahrelangen Ausei-
+				nandersetzungen haben die Polynesier ihre Geheimnisse der Perlenzucht offenbart. Heute, es ist noch
+				möglich, in etwa mehr als einem Dutzend polynesischen Atollen, die nicht überfischt sind, werden Larven
+				der Austern gesammelt. Dies stellt den ersten Vorgang der Perlenzucht dar.
+				<ul class="listItems">
+					<li><a href="#"><img src="images/perlen/perlen_01.jpg" alt="perlen 1" /></a></li>
+					<li><a href="#"><img src="images/perlen/perlen_02.jpg" alt="perlen 2" /></a></li>
+					<li><a href="#"><img src="images/perlen/perlen_03.jpg" alt="perlen 3" /></a></li>
+					<li><a href="#"><img src="images/perlen/perlen_04.jpg" alt="perlen 4" /></a></li>
+					<li><a href="#"><img src="images/perlen/perlen_05.jpg" alt="perlen 5" /></a></li>
+
+					<li><a href="#"><img src="images/perlen/perlen_06.jpg" alt="perlen 6" /></a></li>
+					<li><a href="#"><img src="images/perlen/perlen_07.jpg" alt="perlen 7" /></a></li>
+					<li><a href="#"><img src="images/perlen/perlen_08.jpg" alt="perlen 8" /></a></li>
+					<li><a href="#"><img src="images/perlen/perlen_09.jpg" alt="perlen 9" /></a></li>
+					<li><a href="#"><img src="images/perlen/perlen_10.jpg" alt="perlen 10" /></a></li>
+				<ul>
+			</section>
+		</div>
+	</div>
+</body>
+</html>
