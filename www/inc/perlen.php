7a06e86a748688e3f3be2a994fee4fcb34307b60
jesus <jesus@samsung.(none)>
2013-06-18 14:41:31 -0300

    indentacion en archivos

--- www/inc/perlen.php
+++ www/inc/perlen.php
@@ -1,19 +1,19 @@
-				<h3>Tahiti-Perlen</h3>
-				In den Tiefen der Lagunen des Tuamoto Archipels, abgelegen der Zivilisation der Cook Islands oder den
-				Südprovinzen Chinas, werden die sogenannten "Schwarzen Perlen" gezüchtet. Nach jahrelangen Ausei-
-				nandersetzungen haben die Polynesier ihre Geheimnisse der Perlenzucht offenbart. Heute, es ist noch
-				möglich, in etwa mehr als einem Dutzend polynesischen Atollen, die nicht überfischt sind, werden Larven
-				der Austern gesammelt. Dies stellt den ersten Vorgang der Perlenzucht dar.
-				<ul class="listItems">
-					<li><a href="#"><img src="images/perlen/perlen_01.jpg" alt="perlen 1" /></a></li>
-					<li><a href="#"><img src="images/perlen/perlen_02.jpg" alt="perlen 2" /></a></li>
-					<li><a href="#"><img src="images/perlen/perlen_03.jpg" alt="perlen 3" /></a></li>
-					<li><a href="#"><img src="images/perlen/perlen_04.jpg" alt="perlen 4" /></a></li>
-					<li><a href="#"><img src="images/perlen/perlen_05.jpg" alt="perlen 5" /></a></li>
+<h3>Tahiti-Perlen</h3>
+In den Tiefen der Lagunen des Tuamoto Archipels, abgelegen der Zivilisation der Cook Islands oder den
+Südprovinzen Chinas, werden die sogenannten "Schwarzen Perlen" gezüchtet. Nach jahrelangen Ausei-
+nandersetzungen haben die Polynesier ihre Geheimnisse der Perlenzucht offenbart. Heute, es ist noch
+möglich, in etwa mehr als einem Dutzend polynesischen Atollen, die nicht überfischt sind, werden Larven
+der Austern gesammelt. Dies stellt den ersten Vorgang der Perlenzucht dar.
+<ul class="listItems">
+	<li><a href="#"><img src="images/perlen/perlen_01.jpg" alt="perlen 1" /></a></li>
+	<li><a href="#"><img src="images/perlen/perlen_02.jpg" alt="perlen 2" /></a></li>
+	<li><a href="#"><img src="images/perlen/perlen_03.jpg" alt="perlen 3" /></a></li>
+	<li><a href="#"><img src="images/perlen/perlen_04.jpg" alt="perlen 4" /></a></li>
+	<li><a href="#"><img src="images/perlen/perlen_05.jpg" alt="perlen 5" /></a></li>
 
-					<li><a href="#"><img src="images/perlen/perlen_06.jpg" alt="perlen 6" /></a></li>
-					<li><a href="#"><img src="images/perlen/perlen_07.jpg" alt="perlen 7" /></a></li>
-					<li><a href="#"><img src="images/perlen/perlen_08.jpg" alt="perlen 8" /></a></li>
-					<li><a href="#"><img src="images/perlen/perlen_09.jpg" alt="perlen 9" /></a></li>
-					<li><a href="#"><img src="images/perlen/perlen_10.jpg" alt="perlen 10" /></a></li>
-				<ul>
+	<li><a href="#"><img src="images/perlen/perlen_06.jpg" alt="perlen 6" /></a></li>
+	<li><a href="#"><img src="images/perlen/perlen_07.jpg" alt="perlen 7" /></a></li>
+	<li><a href="#"><img src="images/perlen/perlen_08.jpg" alt="perlen 8" /></a></li>
+	<li><a href="#"><img src="images/perlen/perlen_09.jpg" alt="perlen 9" /></a></li>
+	<li><a href="#"><img src="images/perlen/perlen_10.jpg" alt="perlen 10" /></a></li>
+<ul>
