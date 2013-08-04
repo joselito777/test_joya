2a612ee89f2ea670af9a648ff48b598d385b31d9
Jesus Ojeda <jojeda63@gmail.com>
2013-08-04 11:05:18 -0300

    slider correcciones de css en los controles

--- inc/perlen/tahiti-perlen.php
+++ inc/perlen/tahiti-perlen.php
@@ -1,30 +1,3 @@
-<?
-	$photos = array(
-		'barrakudas-uw.jpg',
-		'broome-harbour.jpg',
-		'broome-pearls.jpg',
-		'garnelle-anemone.jpg',
-		'hai-kubaI.jpg',
-
-		'hutte-rangiroa.jpg',
-		'japanese-cementery-broome.jpg',
-		'nicolasfisch.jpg',
-		'papaleyiii.jpg',
-		'tintenfisch-hp.jpg',
-
-		'rafflesbay-australien-perlenfarm.jpg',
-		'keit-dureau-sam-marlow-kuribayashi.jpg',
-		'rangiroa-strasse.jpg',
-		'robertwan.jpg',
-		'tiburon-rangiroa-ia.jpg',
-		'uw-fisch.jpg',
-		'zuchtung-perle.jpg',
-	);
-
-	$pathImages = $homeRoot.'/images/photos/perlen';
-	$pathThms = $pathImages.'/thumbnails';	
-?>
-
 <h3><?=$dictionarySubSections['perlen']['tahiti-perlen']?></h3>
 <p>
 	In den Tiefen der Lagunen des Tuamoto Archipels, abgelegen der Zivilisation der Cook Islands oder den
